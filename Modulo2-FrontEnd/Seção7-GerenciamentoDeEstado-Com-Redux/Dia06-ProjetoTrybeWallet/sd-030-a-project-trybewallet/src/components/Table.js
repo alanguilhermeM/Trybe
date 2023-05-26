c16ed82f405f68a8918a87c0576a83e9743eb56e
alanguilhermeM <alanguilherme13@outlook.com>
2023-05-26 09:55:34 -0300

    Projeto TrybeWallet

---
+++ Modulo2-FrontEnd/Seção7-GerenciamentoDeEstado-Com-Redux/Dia06-ProjetoTrybeWallet/sd-030-a-project-trybewallet/src/components/Table.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { editExpenses, deleteExpenses } from '../redux/actions';
+import './Table.module.css';
+
+class Table extends Component {
+  deleteExpense = (id) => {
+    const { expenses, dispatch } = this.props;
+    const filtrado = expenses.filter((expense) => expense.id !== id);
+    return dispatch(deleteExpenses(filtrado));
+  };
+
+  render() {
+    const { expenses, dispatch } = this.props;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{expense.description}</td>
+              <td>{expense.tag}</td>
+              <td>{expense.method}</td>
+              <td>{Number(expense.value).toFixed(2)}</td>
+              <td>{expense.exchangeRates[expense.currency].name}</td>
+              <td>{Number(expense.exchangeRates[expense.currency].ask).toFixed(2)}</td>
+              <td>
+                {Number(expense.value * expense.exchangeRates[expense.currency].ask)
+                  .toFixed(2)}
+              </td>
+              <td>Real</td>
+              <td>
+                <button
+                  data-testid="edit-btn"
+                  onClick={ () => {
+                    dispatch(editExpenses(expense.id));
+                  } }
+                >
+                  Editar
+
+                </button>
+                <button
+                  data-testid="delete-btn"
+                  onClick={ () => this.deleteExpense(expense.id) }
+                >
+                  Excluir
+
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    expenses: state.wallet.expenses,
+    idToEdit: state.wallet.idToEdit,
+  };
+}
+
+Table.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
